69e97a4542cacd96ae55ab8044ac3ea282a26d33
Kevin McCartney <hello@kevinmccartney.is>
2019-07-04 19:47:20 -0400

    Getting directories into script

--- certbot/init.sh
+++ certbot/init.sh
@@ -3,8 +3,12 @@ docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d wet
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d www.wethe.party
 docker run -dit --volume=wtp-proxy_certs_data:/etc/letsencrypt --name=cert_container ubuntu tail -f /dev/null
 
-docker cp cert_container:/etc/letsencrypt ../certificates
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-container=(docker ps -aqf "name=cert_container")
+echo "$DIR../certificates";
 
-docker stop $container
+# docker cp cert_container:/etc/letsencrypt $DIR/../certificates
+
+# container=(docker ps -aqf "name=cert_container")
+
+# docker stop $container
