ef031035a057bb5e2582744aad6396e2dc0c4318
Kevin McCartney <hello@kevinmccartney.is>
2019-07-04 18:40:01 -0400

    Save let's encrypt stuff to a volume

--- certbot/init.sh
+++ certbot/init.sh
@@ -1,3 +1,4 @@
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d api.wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d www.wethe.party
+docker images ps -a
