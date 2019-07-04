e7c089b9db80a2af32aa8aef2516660d3b56fdc6
Kevin McCartney <hello@kevinmccartney.is>
2019-07-04 19:15:54 -0400

    update script

--- certbot/init.sh
+++ certbot/init.sh
@@ -1,7 +1,7 @@
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d api.wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d www.wethe.party
-docker run -dit --volume=wtp-proxy_certs_data:/etc/letsencrypt --name=cert_container tail -f /dev/null
+docker run -dit --volume=wtp-proxy_certs_data:/etc/letsencrypt --name=cert_container ubuntu tail -f /dev/null
 
 docker cp cert_container:/etc/letsencrypt ../certificates
 
