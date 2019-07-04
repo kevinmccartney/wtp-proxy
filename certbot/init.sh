d0371a4efe8b46ae0484cd5791df4573d8e9559d
Kevin McCartney <hello@kevinmccartney.is>
2019-07-04 19:00:34 -0400

    It all works

--- certbot/init.sh
+++ certbot/init.sh
@@ -1,4 +1,10 @@
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d api.wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d wethe.party 
 docker-compose run certbot certbot certonly --webroot -w /var/www/certbot -d www.wethe.party
-docker images ps -a
+docker run -dit --volume=wtp-proxy_certs_data:/etc/letsencrypt --name=cert_container ubuntu y
+
+docker cp cert_container:/etc/letsencrypt ../certificates
+
+$container=(docker -aqf "name=cert_container")
+
+docker stop $container
